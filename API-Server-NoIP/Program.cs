45aecc81b0af51c4ba588d81ec002db55d674209
Rian S. Macedo <riansm100@gmail.com>
2025-01-12 06:45:34 -0300

    🌞 adicionado validação de ip | adicionado launch vscode debug

--- API-Server-NoIP/Program.cs
+++ API-Server-NoIP/Program.cs
@@ -1,27 +1,54 @@
 ﻿
 using API_Server_NoIP.Service;
 
-var environment = Environment.GetEnvironmentVariables();
-
-string? userName =  environment["USER_NAME"].ToString();
-string? userPass =  environment["USER_PASSWORD"].ToString();
-string? userDNS =  environment["DNS"].ToString();
-string? url      =  environment["URL"].ToString();
-
-if (userName == null || userPass == null || url == null || userDNS == null)
-    throw new ArgumentNullException("USER_NAME, USER_PASSWORD, DNS or URL is null");
-
-string base64user = Base64Encode($"{userName}:{userPass}");
-string ip = await WebClient.GetIP();
-
-string urlRequest = $"http://{base64user}@{url}?hostname={userDNS}&myip={ip}";
-
-string response = await WebClient.SendData(urlRequest);
-
-Console.WriteLine(response);
-
-static string Base64Encode(string plainText) 
+internal class Program
 {
-    byte[] plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
-    return System.Convert.ToBase64String(plainTextBytes);
+    private static async Task<int> Main(string[] args)
+    {
+        var environment = Environment.GetEnvironmentVariables();
+
+        string path = $"{Directory.GetCurrentDirectory()}/Data";
+        string fullpath = $"{path}/PreviusIp.data";
+
+        string? userName = environment["USER_NAME"].ToString();
+        string? userPass = environment["USER_PASSWORD"].ToString();
+        string? userDNS = environment["DNS"].ToString();
+        string? url = environment["URL"].ToString();
+
+        if (userName == null || userPass == null || url == null || userDNS == null)
+            throw new ArgumentNullException("USER_NAME, USER_PASSWORD, DNS or URL is null");
+
+        string base64user = Base64Encode($"{userName}:{userPass}");
+        string ip = await WebClient.GetIP();
+
+
+        if(!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+
+        bool fileExits = File.Exists(fullpath);
+        if (fileExits){
+            string previusIp = File.ReadAllText(fullpath);
+            if (previusIp.Equals(previusIp))
+            {
+                return 0;
+            }
+        }
+        
+        File.WriteAllText(fullpath, ip);
+        
+        string urlRequest = $"http://{base64user}@{url}?hostname={userDNS}&myip={ip}";
+
+        string response = await WebClient.SendData(urlRequest);
+
+        Console.WriteLine(response);
+
+        return 0;
+    }
+    static string Base64Encode(string plainText)
+    {
+        byte[] plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+        return Convert.ToBase64String(plainTextBytes);
+    }
 }
